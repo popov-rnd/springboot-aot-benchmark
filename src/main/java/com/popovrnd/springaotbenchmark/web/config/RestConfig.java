ab5802d2987738baa93f754a212ba175e229fe88
Popov R&D <andrii@popov-rnd.com>
2026-03-04 12:56:23 +0200

    Added Apach http client

--- src/main/java/com/popovrnd/springaotbenchmark/web/config/RestConfig.java
+++ src/main/java/com/popovrnd/springaotbenchmark/web/config/RestConfig.java
@@ -1,33 +1,42 @@
 package com.popovrnd.springaotbenchmark.web.config;
 
 import com.popovrnd.springaotbenchmark.web.controller.DelayedClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.service.registry.ImportHttpServices;
-
-import java.net.http.HttpClient;
-import java.time.Duration;
 
 @Configuration
 @ImportHttpServices(basePackageClasses = DelayedClient.class)
 public class RestConfig {
 
-    /*@Bean
-    public RestClient restClient() {
+    @Bean
+    public CloseableHttpClient apacheHttpClient() {
 
-        HttpClient httpClient = HttpClient.newBuilder()
-                .version(HttpClient.Version.HTTP_1_1)
-                .connectTimeout(Duration.ofSeconds(5))
+        PoolingHttpClientConnectionManager cm =
+                new PoolingHttpClientConnectionManager();
+
+        cm.setMaxTotal(10000);
+        cm.setDefaultMaxPerRoute(10000);
+
+        return HttpClients.custom()
+                .setConnectionManager(cm)
+                .disableAutomaticRetries()
                 .build();
+    }
 
-        var requestFactory = new JdkClientHttpRequestFactory(httpClient);
-        requestFactory.setReadTimeout(Duration.ofSeconds(10));
+    @Bean
+    public RestClient restClient(CloseableHttpClient apacheHttpClient) {
+
+        HttpComponentsClientHttpRequestFactory factory =
+                new HttpComponentsClientHttpRequestFactory(apacheHttpClient);
 
         return RestClient.builder()
-                .baseUrl("http://127.0.0.1:8081")
-                .requestFactory(requestFactory)
+                .requestFactory(factory)
                 .build();
-    }*/
+    }
 }
