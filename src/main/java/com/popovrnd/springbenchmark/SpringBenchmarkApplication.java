91fcc08da43c8d75e501a7ff6739ad5ff25b7b76
Popov R&D <andrii@popov-rnd.com>
2026-07-20 11:57:34 +0300

    Added missing config annotation

--- src/main/java/com/popovrnd/springbenchmark/SpringBenchmarkApplication.java
+++ src/main/java/com/popovrnd/springbenchmark/SpringBenchmarkApplication.java
@@ -4,9 +4,11 @@ import com.popovrnd.springbenchmark.service.ExternalClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.resilience.annotation.EnableResilientMethods;
 import org.springframework.web.service.registry.ImportHttpServices;
 
 @SpringBootApplication
+@EnableResilientMethods
 @ConfigurationPropertiesScan
 @ImportHttpServices(basePackageClasses = ExternalClient.class)
 public class SpringBenchmarkApplication {
